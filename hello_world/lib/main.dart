cd8b004ac108254b364a156784c77e961c791886
DeukYu <ldy8070@naver.com>
2023-11-27 22:13:56 +0900

    Init

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          body: Center(
+            child: Text(
+              "Hello",
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
+          ))));
+}
