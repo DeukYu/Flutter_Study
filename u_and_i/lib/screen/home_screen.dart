dbd060f972dea652de853fe498f5476933d83637
DeukYu <ldy8070@naver.com>
2023-12-12 23:14:50 +0900

    d-day app

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,109 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Colors.pink[100],
+        body: SafeArea(
+          bottom: false,
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [_TopPart(), _BottomPart()],
+              )),
+        ));
+  }
+}
+
+class _TopPart extends StatefulWidget {
+  const _TopPart({super.key});
+
+  @override
+  State<_TopPart> createState() => __TopPartState();
+}
+
+class __TopPartState extends State<_TopPart> {
+  DateTime selectedDate =
+      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            "U&I",
+            style: TextStyle(
+                color: Colors.white, fontFamily: 'parisienne', fontSize: 80.0),
+          ),
+          Column(children: [
+            Text(
+              "우리 처음 만난날",
+              style: TextStyle(
+                  color: Colors.white, fontFamily: 'sunflower', fontSize: 30.0),
+            ),
+            Text(
+              "${selectedDate.year}.${selectedDate.month}.${selectedDate.day}",
+              style: TextStyle(
+                  color: Colors.white, fontFamily: 'sunflower', fontSize: 20.0),
+            ),
+          ]),
+          IconButton(
+              iconSize: 60.0,
+              onPressed: () {
+                showCupertinoDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    builder: (BuildContext context) {
+                      return Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          color: Colors.white,
+                          height: 300.0,
+                          child: CupertinoDatePicker(
+                            mode: CupertinoDatePickerMode.date,
+                            initialDateTime: selectedDate,
+                            maximumDate: DateTime(now.year, now.month, now.day),
+                            onDateTimeChanged: (DateTime date) {
+                              setState(() {
+                                selectedDate = date;
+                              });
+                            },
+                          ),
+                        ),
+                      );
+                    });
+              },
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+              )),
+          Text(
+            "D+${DateTime(now.year, now.month, now.day).difference(selectedDate).inDays + 1}",
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatefulWidget {
+  const _BottomPart({super.key});
+
+  @override
+  State<_BottomPart> createState() => _BottomPartState();
+}
+
+class _BottomPartState extends State<_BottomPart> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Image.asset('asset/img/middle_image.png'));
   }
 }
