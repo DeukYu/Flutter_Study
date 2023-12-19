f4be8abc7cad6a29295b8e717c8e3bae2da701a6
DeukYu <ldy8070@naver.com>
2023-12-20 00:16:51 +0900

    random_number_generator

---
+++ random_number_generator/lib/constant/color.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const Color PRIMARY_COLOR = Color(0xFE2D2D33);
+const Color RED_COLOR = Color(0xFFEA4955);
+const Color BLUE_COLOR = Color(0xFE549FBF);
---
+++ random_number_generator/lib/screen/home_screen.dart
@@ -0,0 +1,118 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<int> randomNumbers = [123, 456, 789];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: PRIMARY_COLOR,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const _Header(),
+                _Body(randomNumbers: randomNumbers),
+                _Footer(onPressed: onRandomNumberGenerate)
+              ],
+            ),
+          ),
+        ));
+  }
+
+  onRandomNumberGenerate() {
+    final rand = Random();
+
+    final Set<int> newNumbers = {};
+    while (newNumbers.length != 3) {
+      final number = rand.nextInt(1000);
+      newNumbers.add(number);
+      print(number);
+    }
+
+    setState(() {
+      randomNumbers = newNumbers.toList();
+    });
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "랜덤숫자 생성기",
+          style: TextStyle(
+              color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.w700),
+        ),
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.settings,
+              color: RED_COLOR,
+            )),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNumbers;
+  const _Body({required this.randomNumbers});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: randomNumbers
+          .asMap()
+          .entries
+          .map((x) => Padding(
+                padding: EdgeInsets.only(bottom: x.key == 2 ? 0 : 16.0),
+                child: Row(
+                  children: x.value
+                      .toString()
+                      .split('')
+                      .map((y) => Image.asset(
+                            'asset/img/$y.png',
+                            height: 70.0,
+                            width: 50.0,
+                          ))
+                      .toList(),
+                ),
+              ))
+          .toList(),
+    ));
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+  const _Footer({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+            onPressed: onPressed,
+            child: Text("생성하기")));
+  }
+}
