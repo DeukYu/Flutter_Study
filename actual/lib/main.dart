a7d1177288d1a8863db00860f71919bf4eda384b
DeukYu <ldy8070@naver.com>
2024-01-12 01:07:01 +0900

    UI 작업 완료

--- actual/lib/main.dart
+++ actual/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:actual/common/component/custom_text_form_field.dart';
+import 'package:actual/user/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,21 +13,11 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomTextFormField(
-            hintText: "이메일을 입력해주세요.",
-            onChanged: (String value) {},
-          ),
-          CustomTextFormField(
-              hintText: "비밀번호를 입력해주세요.",
-              obscureText: true,
-              onChanged: (String value) {})
-        ],
-      )),
+      home: LoginScreen(),
     );
   }
 }
